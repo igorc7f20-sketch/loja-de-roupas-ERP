f75aa4f59c464704d4b5c0aeca0bf14aedbcad61
igorc7f20-sketch <igorc7f20@gmail.com>
2025-12-10 21:45:06 -0400

    Força detecção de testes com config ajustada

--- backend/erp/tests.py
+++ backend/erp/tests.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
 
-
 class SmokeTest(TestCase):
-    def test_math_works(self):
-        """Um teste simples para garantir que o pytest roda"""
-        self.assertEqual(1 + 1, 2)
+    def test_soma_simples(self):
+        assert 1 + 1 == 2
